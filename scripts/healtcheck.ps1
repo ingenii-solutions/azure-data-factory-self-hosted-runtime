33efa19081b1d3a94374df1153d100de326be110
Teodor Kostadinov <teodor@privatemail.us>
2021-08-27 20:37:55 +0000

    save: checkpoint

--- scripts/healtcheck.ps1
+++ scripts/healtcheck.ps1
@@ -1,22 +1,4 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# IMPORTS
-# ---------------------------------------------------------------------------------------------------------------------
-. "$PSScriptRoot\common.ps1"
-
-function Test-NodeConnection() {
-    $outputFile = "status-check.txt"
-
-    Start-Process $DmgcmdPath -Wait -ArgumentList "-cgc" -RedirectStandardOutput $outputFile
-    
-    $ConnectionResult = Get-Content $outputFile
-    
-    Remove-Item -Force $outputFile
-
-    if ($ConnectionResult -like "Connected") {
-        return $true
-    }
-
-    $false
-}
+# Load the library of functions
+. "$PSScriptRoot\lib.ps1"
 
 if (!Test-NodeConnection) { exit 1 }
--- scripts/lib.ps1
+++ scripts/lib.ps1
@@ -42,3 +42,19 @@ function Clear-Workdir() {
 
     Get-ChildItem $Workdir | Where-Object { $_.Name -Match "IntegrationRuntime.*.msi" } | Remove-Item -Force
 }
+
+function Test-NodeConnection() {
+    $outputFile = "status-check.txt"
+
+    Start-Process $DmgcmdPath -Wait -ArgumentList "-cgc" -RedirectStandardOutput $outputFile
+    
+    $ConnectionResult = Get-Content $outputFile
+    
+    Remove-Item -Force $outputFile
+
+    if ($ConnectionResult -like "Connected") {
+        return $true
+    }
+
+    $false
+}
